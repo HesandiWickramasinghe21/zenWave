e7d9bac04d6fcf84d9260cd9bec371f23d91cc8c
Sharan2122 <mohmed.20242122@iit.ac.lk>
2026-03-13 19:38:26 +0530

    Refactor _selectDate method for improved readability and structure

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -12,7 +12,23 @@ class _SignupScreenState extends State<SignupScreen> {
 
   String? _selectedGender;
 
-  Future<void> _selectDate(BuildContext context) async 
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+
+      initialDate: DateTime.now().subtract(const Duration(days: 6570)),
+
+      firstDate: DateTime(1900),
+
+      lastDate: DateTime.now(),
+    );
+
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
 
   Widget _buildTextField(
     String label,
