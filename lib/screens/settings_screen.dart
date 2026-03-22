cf50b26d3c8017e58e31795c33d3c44153f189bd
Hesandi Wickramasinghe <hesandi.20242159@ii.ac.lk>
2026-03-23 06:13:00 +0800

    docs: add header comment to settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 // Settings screen: manages user preferences and reminders
+// Settings screen: manages user preferences and reminders
 import '../services/local_storage.dart';
 import '../services/reminder_service.dart';
 import '../services/api_service.dart';
